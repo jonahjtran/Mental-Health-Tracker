ffcb32f95155e6835c5ffbf19e3be1456930690e
Jonah Tran <jonahjtran@gmail.com>
2026-01-04 22:40:43 +0800

    created session.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -1,15 +1,9 @@
-from typing import List
-from typing import Optional
-from sqlalchemy import ForeignKey
-from sqlalchemy import String
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.orm import Mapped
-from sqlalchemy.orm import mapped_column
-from sqlalchemy.orm import relationship
+from typing import List, Optional
+from sqlalchemy import ForeignKey, String
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from datetime import date, datetime
+from base import Base
 
-class Base(DeclarativeBase):
-    pass
 
 
 class Users(Base):
---
+++ backend/app/db/session.py
@@ -0,0 +1,16 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from collections.abc import Generator
+from app.core.config import settings
+from sqlalchemy.orm import Session
+
+SQLALCHEMY_DATABASE_URL = settings.database_url  # from app.core.config
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=False)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+def get_db() -> Generator[Session, None, None]:
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
