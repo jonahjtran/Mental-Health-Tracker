4e93935e9a055804e83499cfd6d479a64bf21151
Jonah Tran <jonahjtran@gmail.com>
2026-07-10 16:06:41 -0500

    prepping for vercel

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -5,7 +5,7 @@ from backend.app.core.config import settings
 from sqlalchemy.orm import Session
 
 SQLALCHEMY_DATABASE_URL = settings.database_url  # from backend.app.core.config
-engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=False)
+engine = create_engine(SQLALCHEMY_DATABASE_URL, future=True, echo=False, pool_pre_ping=True)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 def get_db() -> Generator[Session, None, None]:
